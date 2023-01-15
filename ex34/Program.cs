fa9248074890af1d094684b7e43211f4acc9c4c8
Vlad A <seralesin@mail.ru>
2023-01-15 17:35:03 +0300

    сделал первую задачу

---
+++ ex34/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2 */
+
+// метод, который считывает с консоли (для размерности массива + границы случ. чисел)
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//метод для инициализации массива рандомными числами
+int[] InitArray(int dimension, int rightBound, int leftBound)
+{
+    int[] result = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        result[i] = rnd.Next(leftBound, rightBound);
+    }
+    return result;
+}
+
+//метод для печати массива
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// поиск четных чисел
+int CountEven(int[] array)
+{
+    int j = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            j++;
+    }
+    return j;
+}
+
+int dimensin = GetNumber("Введите размерность массива");
+int leftBound = GetNumber("Введите левую границу значений");
+int rightBound = GetNumber("Введите правую границу значений");
+
+int[] arr = InitArray(dimensin, leftBound, rightBound);
+
+PrintArray(arr);
+
+Console.WriteLine(CountEven(arr));
