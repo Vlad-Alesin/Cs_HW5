36bb266e132001d5358f39fd4ddd5c169afb3e6b
Vlad A <seralesin@mail.ru>
2023-01-15 17:44:16 +0300

    выполнил

---
+++ ex36/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+[3, 7, 23, 12] -> 19
+
+[-4, -6, 89, 6] -> 0 */
+
+// метод, который считывает с консоли (для размерности массива + границы случ. чисел)
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число");
+        }
+    }
+
+    return result;
+}
+
+//метод для инициализации массива рандомными числами
+int[] InitArray(int dimension, int leftBound, int rightBound)
+{
+    int[] result = new int[dimension];
+    Random rnd = new Random();
+
+    for (int i = 0; i < dimension; i++)
+    {
+        result[i] = rnd.Next(leftBound, rightBound);
+    }
+    return result;
+}
+
+//метод для печати массива
+void PrintArray(int [] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+// поиск четных чисел
+int CountSum(int[] array)
+{
+    int j = 0;
+    for (int i=0; i < array.Length; i++)
+    {
+        if (i % 2 != 0)
+            j += array[i];
+    }
+    return j;
+}
+
+int dimensin = GetNumber("Введите размерность массива");
+int leftBound = GetNumber("Введите левую границу значений");
+int rightBound = GetNumber("Введите правую границу значений");
+
+int[] arr = InitArray(dimensin, leftBound, rightBound);
+
+PrintArray(arr);
+
+Console.WriteLine(CountSum(arr));
